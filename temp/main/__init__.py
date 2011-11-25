6348cff9c9248988305e04532336a71e876d3c36
root <root@NekoNami.(none)>
2011-11-24 19:45:08 -0500

    Cleaned up Folder

--- temp/main/__init__.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-@register.filter(name='currency')
-def currency(decimal):
-    dollars = float(dollars)
-    return "$%s%s" % (int(dollars), ("%0.2f" % dollars)[-3:])
-
