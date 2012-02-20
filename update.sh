344fc0fb0c058c4ca2f6ccd6f295b63c4185d9e0
Kevin Anthony <kevin.s.anthony@gmail.com>
2012-02-20 13:57:30 -0500

    added dependincies to update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+#update dependancies
+pip install --upgrade -e git://github.com/Fantomas42/django-blog-zinnia.git#egg=django-blog-zinnia
+pip install --upgrade south
+rm -rf src
+rm -rf build
+
+#update main program
 git update
+
+#cleanup
 ./migrate.sh
 ./clean.sh
 service apache2 restart
